557d8b0c22f0189e06b873f049cd593fdd769983
duhenduhen <duhenduhenduhen@gmail.com>
2025-09-05 23:04:18 +0300

    tone-common: Build android.hidl.base@1.0.vendor

--- tone.mk
+++ tone.mk
@@ -244,6 +244,7 @@ PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
 
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
+    android.hidl.base@1.0.vendor\
     android.hidl.manager@1.0 \
     libhidltransport \
     libhidltransport.vendor \
