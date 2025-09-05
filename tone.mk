e9d404aa9b9569463c19c64453decaf71663394b
Bruno Martins <bgcngm@gmail.com>
2025-09-05 08:09:55 +0300

    tone-common: Drop default DRM service
    This is not needed, since Widevine DRM and Clearkey DRM
    services are available.
    
    Change-Id: I97023f1bab88ddb8fa4cb205df190c516a759951

--- tone.mk
+++ tone.mk
@@ -192,8 +192,6 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
     android.hardware.drm@1.1.vendor \
     android.hardware.drm@1.4-service.clearkey
 
