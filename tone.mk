3bbe0267334263fc8822012a2841d438827aa25a
im Zimmermann <tim@linux4.de>
2025-09-12 22:02:35 +0300

    tone-common: Inherit non_ab_device.mk
    * This is required to build applypatch for updating recovery
    
    Change-Id: I38053691f7724b5d6c30159c410d5d04ac136180

--- tone.mk
+++ tone.mk
@@ -16,6 +16,7 @@
 #
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/sony/tone-common/tone-common-vendor.mk)
